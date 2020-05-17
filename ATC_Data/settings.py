9f7ea86f57d22357a22bb219819a6821f0afafba
machao1995 <1598214715@qq.com>
2020-05-17 19:10:05 +0800

    5月17日

--- ATC_Data/settings.py
+++ ATC_Data/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 或者使用 mysql.connector.django
         'NAME': 'atc',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '12345678',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
