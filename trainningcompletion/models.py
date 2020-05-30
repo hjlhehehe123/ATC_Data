46316a1c1e365e0f401a1a1d180732b2ff978dd9
machao1995 <1598214715@qq.com>
2020-05-30 13:58:41 +0800

    5.30

--- trainningcompletion/models.py
+++ trainningcompletion/models.py
@@ -18,3 +18,22 @@ class trainningstatusdetail(models.Model):
     frontdata14 = models.CharField(max_length=300)
     frontdata15 = models.CharField(max_length=300)
     frontdata16 = models.CharField(max_length=300)
+
+    # 这是其他培训的，other表示其他培训
+class trainningstatusdetailother(models.Model):
+    frontdata1 = models.CharField(max_length=300)
+    frontdata2 = models.CharField(max_length=300)
+    frontdata3 = models.CharField(max_length=300)
+    frontdata4 = models.CharField(max_length=300)
+    frontdata5 = models.CharField(max_length=300)
+    frontdata6 = models.CharField(max_length=300)
+    frontdata7 = models.CharField(max_length=300)
+    frontdata8 = models.CharField(max_length=300)
+    # frontdata9 = models.CharField(max_length=300)
+    frontdata10 = models.CharField(max_length=300)
+    frontdata11 = models.CharField(max_length=300)
+    frontdata12 = models.CharField(max_length=300)
+    frontdata13 = models.CharField(max_length=300)
+    frontdata14 = models.CharField(max_length=300)
+    frontdata15 = models.CharField(max_length=300)
+    frontdata16 = models.CharField(max_length=300)
--- trainningcompletion/urls.py
+++ trainningcompletion/urls.py
@@ -2,12 +2,23 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
+# 这是模拟机培训的
 url(r'^addtrainningrecord/$', views.addtrainningrecord, name="addtrainningrecord"),
 url(r'^addtrainningrecord1/$', views.addtrainningrecord1, name="addtrainningrecord1"),
 url(r'^ajax_add/$', views.ajax_add),
 url(r'^ajax_addtrainningrecord/$', views.ajax_addtrainningrecord),
-url(r'^ajax_addtrainningrecord2/$', views.ajax_addtrainningrecord),
+url(r'^ajax_addtrainningrecord2/$', views.ajax_addtrainningrecord2),
 url(r'^trainningstatusdetail/$', views.trainningstatusdetail, name="trainningstatusdetail"),
+
+
+# 这是其他培训的
+url(r'^addtrainningrecordother/$', views.addtrainningrecordother, name="addtrainningrecordother"),
+url(r'^addtrainningrecordother1/$', views.addtrainningrecordother1, name="addtrainningrecordother1"),
+url(r'^ajax_addother/$', views.ajax_addother),
+url(r'^ajax_addtrainningrecordother/$', views.ajax_addtrainningrecordother),
+
+url(r'^trainningstatusdetailother/$', views.trainningstatusdetailother, name="trainningstatusdetailother"),
 url(r'^test/$', views.test, name="test"),
 
 ]
